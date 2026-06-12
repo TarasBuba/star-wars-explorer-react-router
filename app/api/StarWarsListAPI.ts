0dc911fd93cc32fc35e57b6c418a99a1f5d5c2d3
TarasBuba <progibov2022@gmail.com>
2026-06-11 19:09:44 -0600

    fix: useAsync, SWApi

--- app/api/StarWarsListAPI.ts
+++ app/api/StarWarsListAPI.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const StarWarsListAPI = async (resource: string) => {
-  const results = await axios.get(
-    `https://star-wars-api-bi5l.onrender.com/${resource}`
-  );
-  return results.data.data;
+  const results = await axios
+    .get(`https://star-wars-api-bi5l.onrender.com/${resource}`)
+    .then(({ data }) => data);
+  return results.data;
 };
 
 export default StarWarsListAPI;
