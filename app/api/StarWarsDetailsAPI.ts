2f7d5594a00b6cddcb8d26454f67a73a75cf950a
TarasBuba <progibov2022@gmail.com>
2026-06-11 18:53:34 -0600

    Update app/api/StarWarsDetailsAPI.ts
    Co-authored-by: Vadym Lohvynovych <37845039+vadym-lohvynovych@users.noreply.github.com>

--- app/api/StarWarsDetailsAPI.ts
+++ app/api/StarWarsDetailsAPI.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const StarWarsDetailsAPI = async (resource: string, id: string) => {
+const StarWarsDetailsAPI = async (resource: string, id: string) => axios.get(`https://star-wars-api-bi5l.onrender.com/api/v1/${resource}/${id}`).then(({data}) => data)
   const results = await axios.get(
     `https://star-wars-api-bi5l.onrender.com/api/v1/${resource}/${id}`
   );
